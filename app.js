e1169d566def03e2cf7bcd5aa04c88e83653b2fc
Baron Chong <baronchong@softmobile.com.tw>
2021-08-02 17:10:47 +0800

    [create] : Campground Index

--- app.js
+++ app.js
@@ -25,11 +25,10 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'chaep camping!'});
-    await camp.save();
-    res.send(camp)
-});
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
