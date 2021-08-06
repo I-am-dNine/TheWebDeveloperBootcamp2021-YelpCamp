fc2b291fa8c8ecbd3cd743b4e74120807095e174
Baron Chong <baronchong@softmobile.com.tw>
2021-08-06 17:51:35 +0800

    no message

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    usseCreateIndex: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 });
