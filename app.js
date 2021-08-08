2b56fbd36f796c2df30403e1e8e87a9f0a323fc5
Baron Chong <baronchong@softmobile.com.tw>
2021-08-08 16:55:43 +0800

    [create] : Register Form

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -64,13 +64,10 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = User({ email: 'dnine@gmail.com', username: 'dnine'});
-    const newUser = await User.register(user, 'iamdnine');
-    res.send(newUser);
-})
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+
+app.use('/', userRoutes) 
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 
 app.get('/', (req, res) => {
