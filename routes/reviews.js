e128c75705a75dfd1ea1b8630c08bef895faca93
Baron Chong <baronchong@softmobile.com.tw>
2021-08-11 16:15:08 +0800

    [add] : Reviews Permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
+    review.author = req.user._id;
     await review.save();
     await campground.save();
     req.flash('success', 'Created new review!');
